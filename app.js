477287bc6a9af03f6edfe5d836b93d759732b39c
ajboi <aj.kannan1101@gmail.com>
2022-01-22 11:24:38 +0530

    Globals.

---
+++ app.js
@@ -0,0 +1,10 @@
+/* Node Globals 
+Set of predefined objects that lets us work with the file system and everything.
+Analogous to the window and the document objects in browser.
+
+__dirname - path to current directory.
+__filename - file name.
+require - function to use modules (commonJS).
+module - info about current module (file).
+process - info about the environment where the program is being executed.
+console - same as the browser console.
