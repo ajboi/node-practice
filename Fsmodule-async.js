1f86fcc7de58ced06909e8c970b4f4abd2c7b7a8
ajboi <aj.kannan1101@gmail.com>
2022-01-22 21:26:02 +0530

    fs async.

---
+++ Fsmodule-async.js
@@ -0,0 +1,29 @@
+const { readFile, writeFile } = require("fs");
+
+// We make these calls asyncronous by using callbacks.
+
+readFile("./test-pathModule/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./test-pathModule/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./test-pathModule/result-async.txt",
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("done.");
+      }
+    );
+  });
+});
